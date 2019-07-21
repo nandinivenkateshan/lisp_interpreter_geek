fb7194ed954ae57abd9dc67df769448b9e089d7d
nandinivenkateshan <nandinivenkatesan809@gmail.com>
2019-07-21 21:22:24 +0530

    refatcored

--- lisp.js
+++ lisp.js
@@ -1,12 +1,11 @@
-let localObject
 
-const env = {
+const globalObj = {
   '+': (...operands) => operands.reduce((acc, item) => (acc + item)),
   '-': (...operands) => operands.reduce((acc, item) => (acc - item)),
   '*': (...operands) => operands.reduce((acc, item) => (acc * item)),
   '/': (...operands) => {
     if (operands.length === 1) return 1 / operands[0]
-    else return operands.reduce((acc, item) => (acc / item))
+    return operands.reduce((acc, item) => (acc / item))
   },
   '>': (left, right) => left > right,
   '<': (left, right) => left < right,
@@ -36,8 +35,10 @@ const numberParse = input => {
     let num = input.match(decimalInfinity)
     let index = num[0].length
     return [num[0] * 1, input.slice(index)]
-  } else return null
+  }
+  return null
 }
+
 const evaluater = expr => {
   let count1 = 0
   let count2 = 0
@@ -46,46 +47,50 @@ const evaluater = expr => {
     if (expr[i] === ')') count2++
   }
   if (count1 !== count2) return null
-  let result = sExpression(expr)
+  let result = sExpression(expr, globalObj)
   return (result === null) ? null : result[0]
 }
-const sExpression = expr => {
+
+const sExpression = (expr, env = globalObj) => {
   expr = expr.trim()
   if (expr.startsWith('(')) {
-    let result = specialFormParser(expr) || expressionParse(expr)
+    let result = specialFormParser(expr, env) || expressionParse(expr, env)
     return (result !== null) ? result : null
-  } else return null
+  }
+  return null
 }
 
-const expressionParse = expr => {
+const expressionParse = (expr, env = globalObj) => {
   expr = expr.trim()
   expr = expr.slice(1)
   let firstVal = expr.slice(0, expr.indexOf(' '))
-  if (env.hasOwnProperty(firstVal)) return evaluateExpr(expr)
-  if (firstVal === 'begin') return beginParse(expr)
-  if (firstVal === 'if') return ifParser(expr)
-  else return null
+  if (globalObj[firstVal]) return evaluateExpr(expr, env)
+  if (firstVal === 'begin') return beginParse(expr, env)
+  if (firstVal === 'if') return ifParser(expr, env)
+  return null
 }
 
-const specialFormParser = expr => {
+const specialFormParser = (expr, env = globalObj) => {
   expr = expr.trim()
   expr = expr.slice(1)
   let firstVal = expr.slice(0, expr.indexOf(' '))
-  if (firstVal === 'define') return defineParser(expr)
-  if (firstVal === 'lambda') return lambdaParser(expr)
-  else return null
+  if (firstVal === 'define') return defineParser(expr, env)
+  if (firstVal === 'lambda') return lambdaParser(expr, env)
+  return null
 }
-const exprParser = input => {
+
+const exprParser = (input, env) => {
   let parser = [booleanParse, numberParse, symbolParse, operatorParse, identifierParser]
   for (let parserFunc of parser) {
-    let result = parserFunc(input)
+    let result = parserFunc(input, env)
     if (result !== null) return result
   }
   return null
 }
+
 const operatorParse = input => {
   let op = input.slice(0, input.indexOf(' '))
-  if (!env[op]) return null
+  if (!globalObj[op]) return null
   return [op, input.slice(op.length)]
 }
 
@@ -95,10 +100,11 @@ const identifierParser = string => {
     let match = string.match(alphabets)
     let index = match[0].length
     return [match[0], string.slice(index)]
-  } else return null
+  }
+  return null
 }
 
-const defineParser = expr => {
+const defineParser = (expr, env = globalObj) => {
   expr = expr.slice(6)
   expr = expr.trim()
   let key
@@ -107,8 +113,7 @@ const defineParser = expr => {
   if (result === null) return null
   key = result[0]
   let val = result[1].trim()
-  if (val.startsWith('(')) res = sExpression(val)
-  else res = exprParser(val) || expressionParse(val)
+  res = sExpression(val, env) || exprParser(val, env) || expressionParse(val, env)
   if (res === null) return null
   if (res[0] === 'define') return [env['define']]
   env[key] = env[res[0]] || res[0]
@@ -118,34 +123,36 @@ const defineParser = expr => {
   expr = expr.trim()
   return ['Added value in env', expr]
 }
-const lambdaParser = expr => {
+
+const lambdaParser = (expr, env = globalObj) => {
   expr = expr.slice(6).trim()
-  localObject = {}
+  let localObj = {}
   if (expr.startsWith('(')) expr = expr.slice(1)
   while (!expr.startsWith(')')) {
     let result = identifierParser(expr)
-    localObject[result[0]] = null
+    localObj[result[0]] = null
     expr = result[1]
   }
   expr = expr.slice(1).trim()
-  localObject.expression = expr
-  return [localObject, localObject.expression]
+  localObj.expression = expr
+  localObj.parent = env
+  return [localObj, localObj.expression]
 }
-const evaluateExpr = expr => {
+
+const evaluateExpr = (expr, env) => {
   let resultArr
   let operator
   let opArr = []
   while (!expr.startsWith(')')) {
     expr = expr.trim()
-    if (expr.startsWith('(')) resultArr = sExpression(expr)
-    else resultArr = exprParser(expr)
+    resultArr = sExpression(expr, env) || exprParser(expr, env)
     if (resultArr === null || resultArr === undefined) return null
-    if (env.hasOwnProperty(resultArr[0]) && (typeof env[resultArr[0]] === 'object')) {
-      return lambdaEval(resultArr)
+    if (globalObj[resultArr[0]] && (typeof globalObj[resultArr[0]] === 'object')) {
+      return lambdaEval(resultArr, env)
     }
-    if (env.hasOwnProperty(resultArr[0]) && ((typeof env[resultArr[0]]) === 'function')) operator = env[resultArr[0]]
-    if (env.hasOwnProperty(resultArr[0]) && ((typeof env[resultArr[0]]) === 'number')) opArr.push(env[resultArr[0]])
-    if (!env.hasOwnProperty(resultArr[0])) opArr.push(resultArr[0])
+    if (globalObj[resultArr[0]] && ((typeof globalObj[resultArr[0]]) === 'function')) operator = globalObj[resultArr[0]]
+    if (globalObj[resultArr[0]] && ((typeof globalObj[resultArr[0]]) === 'number')) opArr.push(globalObj[resultArr[0]])
+    if (!globalObj[resultArr[0]]) opArr.push(resultArr[0])
     expr = resultArr[1]
     if (expr === '') return null
     if (expr.startsWith(' ')) {
@@ -156,40 +163,39 @@ const evaluateExpr = expr => {
   if (!operator) return [...opArr, expr.slice(1)]
   if (typeof operator === 'function') return [operator(...opArr), expr.slice(1)]
 }
-const symbolParse = input => {
+
+const symbolParse = (input, obj) => {
   let res = identifierParser(input)
   if (res !== null) {
-    if (localObject) {
-      if (localObject.hasOwnProperty(res[0])) {
-        let key = Object.keys(localObject)[0]
-        res[0] = localObject[key]
-        return [res[0], res[1]]
-      }
-    }
-    if (env.hasOwnProperty(res[0])) return res
-    return null
-  } else return null
+    if (typeof globalObj[res[0]] === 'object') return res
+    if (obj.parent === undefined) return null
+    let key = Object.keys(obj)[0]
+    res[0] = obj[key]
+    return [res[0], res[1]]
+  }
+  return null
 }
-const lambdaEval = input => {
+
+const lambdaEval = (input, env) => {
   let result
   let expression
-  if (env.hasOwnProperty(input[0])) {
-    let obj = env[input[0]]
-    if (input[1].trim().startsWith('(')) result = sExpression(input[1].trim())
-    else result = exprParser(input[1].trim()) || expressionParse(input[1].trim())
+  if (globalObj[input[0]]) {
+    let obj = globalObj[input[0]]
+    result = sExpression(input[1].trim(), env) || exprParser(input[1].trim(), env) || expressionParse(input[1].trim(), env)
     let arg = Object.keys(obj)[0]
     obj[arg] = result[0]
     expression = obj[Object.keys(obj)[1]]
-    return sExpression(expression)
-  } else return null
+    return sExpression(expression, obj)
+  }
+  return null
 }
 
-const beginParse = (expr) => {
+const beginParse = (expr, env = globalObj) => {
   expr = expr.slice(5)
   let arr = []
   while (!expr.startsWith(')')) {
     expr = expr.trim()
-    let res = sExpression(expr)
+    let res = sExpression(expr, env)
     if (res === null) break
     expr = expr.trim()
     arr.push(res[0])
@@ -200,7 +206,7 @@ const beginParse = (expr) => {
   return [arr[n]]
 }
 
-const ifParser = (expr) => {
+const ifParser = (expr, env = globalObj) => {
   expr = expr.slice(2)
   expr = expr.trim()
   let x
@@ -214,44 +220,36 @@ const ifParser = (expr) => {
       x = i
     }
   }
-  if (expr.startsWith('(')) res = expressionParse(expr)
-  else res = exprParser(expr)
+  res = expressionParse(expr, env) || exprParser(expr, env)
   let val = res[0]
   let condition = res[1].trim()
   if (!condition.startsWith('(')) {
-    let value = exprParser(condition)
+    let value = exprParser(condition, env)
     let value2
     if (val === true) return [value[0], expr.slice(x)]
-    else {
-      if (value[1].trim().startsWith('(')) value2 = expressionParse(value[1].trim())
-      else value2 = exprParser(value[1].trim())
-      return [value2[0], expr.slice(x)]
-    }
-  } else {
-    let index
-    let openBrac = 0
-    let closeBrac = 0
-    let value2
-    for (let i = 0; i < condition.length; i++) {
-      if (condition[i] === '(') openBrac++
-      if (condition[i] === ')') closeBrac++
-      if (openBrac === closeBrac) {
-        index = i
-        break
-      }
-    }
-    let condition1 = condition.slice(0, index + 1)
-    let condition2 = condition.slice(index + 2, condition.length)
-    if (val === true) return [expressionParse(condition1), expr.slice(x)]
-    else {
-      if (condition2.trim().startsWith('(')) value2 = expressionParse(condition2.trim())
-      else value2 = exprParser(condition2.trim())
-      return [value2[0], expr.slice(x)]
+    value2 = expressionParse(value[1].trim(), env) || exprParser(value[1].trim(), env)
+    return [value2[0], expr.slice(x)]
+  }
+  let index
+  let openBrac = 0
+  let closeBrac = 0
+  let value2
+  for (let i = 0; i < condition.length; i++) {
+    if (condition[i] === '(') openBrac++
+    if (condition[i] === ')') closeBrac++
+    if (openBrac === closeBrac) {
+      index = i
+      break
     }
   }
+  let condition1 = condition.slice(0, index + 1)
+  let condition2 = condition.slice(index + 2, condition.length)
+  if (val === true) return [expressionParse(condition1, env), expr.slice(x)]
+  value2 = expressionParse(condition2.trim(), env) || exprParser(condition2.trim(), env)
+  return [value2[0], expr.slice(x)]
 }
 
-console.log(evaluater('(* 1)'))
+console.log(evaluater('(+ 1)'))
 console.log(evaluater('(/ 40)'))
 console.log(evaluater('(/ 40 400)'))
 console.log(evaluater('(+ 1 3 4 6 8 9)'))
@@ -263,7 +261,7 @@ console.log(evaluater('(+ 10 (sqrt 100))'))
 console.log(evaluater('(begin (* 1 2) (* 3 7))'))
 console.log(evaluater('(begin (begin (+ 1 2) (+ 3 7)))'))
 
-console.log(evaluater('(begin (define r 10)(* pi (* r r)))'))
+console.log(evaluater('(begin (define r 10) (* pi (* r r)))'))
 console.log(evaluater('(begin (define e 1) (+ e 3))'))
 console.log(evaluater('(define plus +)'))
 console.log(evaluater('(plus 10 20)'))
@@ -285,7 +283,7 @@ console.log(evaluater('(+ r r)'))
 console.log(evaluater('(define twice (lambda (x) (+ (* 2 x) (+ 2 x))))'))
 console.log(evaluater('(twice 5)'))
 console.log(evaluater('(define twice (lambda (x) (* 2 x)))'))
-console.log(evaluater('(twice (twice (twice 5)))'))
+console.log(evaluater('(twice 5)'))
 
 console.log(evaluater('(define circle-area (lambda (r) (* pi (* r r))))'))
 console.log(evaluater('(circle-area 10)'))
@@ -296,4 +294,3 @@ console.log(evaluater('(define fact (lambda (n) (if (<= n 1) 1 (* n (fact (- n 1
 console.log(evaluater('(fact 3)'))
 console.log(evaluater('(define fact (lambda (n) (if (<= n 1) 1 (* n (fact (- n 1))))))'))
 console.log(evaluater('(fact (fact 3))'))
-
